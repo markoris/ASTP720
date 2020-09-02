80e7dd9c657caf592be4403a923d2e4d3f022f97
Marko Ristic <mr3364@rit.edu>
2020-09-02 13:07:02 -0400

    added Newton's and Secant root-finding methods

--- HW1/root_finding_algos.py
+++ HW1/root_finding_algos.py
@@ -1,4 +1,4 @@
-def bisection(func, lower, upper, eps=0.1, verbose=False, iters=0):
+def bisection(func, lower, upper, eps=1e-8, verbose=False, iters=0):
 	'''
 	Bisection method root-finding algorithm.
 
@@ -21,14 +21,68 @@ def bisection(func, lower, upper, eps=0.1, verbose=False, iters=0):
 
 	if abs(func_root) < eps:
 		if verbose: 
-			print("Root found at %f after %d iterations." % (root, iters))
+			print("Root found at %.16f after %d iterations." % (root, iters))
 			return root, iters
 		return root
 
-	else:
-		if func_lower*func_root < 0: upper = root
-		else: lower = root
-		iters += 1
-		bisection(func, lower, upper, eps, verbose, iters)
+	iters += 1
+	if func_lower*func_root < 0: return bisection(func, lower, root, eps, verbose, iters)
+	else: return bisection(func, root, upper, eps, verbose, iters)
 
-	
+def newton(func, func_deriv, root, eps=1e-8, verbose=False, iters=0):
+	'''
+	Newton's method root-finding algorithm.
+
+	func : function object
+		The function whose root is being found.
+	func_deriv : function object
+		The functional form of the derivative of func.
+	root : float
+		Initial guess for the root value.
+	eps: float
+		Threshold under which root-finding ceases if |func(root)| < eps.
+	verbose: bool
+		Flag which dictates whether number of iterations is printed to stdout and returned.
+	iters: int
+		Number of algorithm iterations required to find the root to within the threshold value.
+	'''
+	func_root = func(root)
+	if abs(func_root) < eps:
+		if verbose: 
+			print("Root found at %.16f after %d iterations." % (root, iters))
+			return root, iters
+		return root
+#	else:
+	next_root = lambda x: x - float(func(x))/float(func_deriv(x))
+	iters += 1
+	return newton(func, func_deriv, next_root(root), eps, verbose, iters)
+
+def secant(func, lower, upper, eps=1e-8, verbose=False, iters=0):
+	'''
+	Secant method root-finding algorithm.
+
+	func : function object
+		The function whose root is being found.
+	lower : float
+		Lower value for the initial secant.
+	upper: float
+		Upper value for the initial secant.
+	eps: float
+		Threshold under which root-finding ceases if |func(root)| < eps.
+	verbose: bool
+		Flag which dictates whether number of iterations is printed to stdout and returned.
+	iters: int
+		Number of algorithm iterations required to find the root to within the threshold value.
+	'''
+	func_lower, func_upper = func(lower), func(upper)
+	next_root = lambda x_im1, x_i: x_i - func(x_i)*(x_i - x_im1)/float(func(x_i)-func(x_im1))
+	root = next_root(lower, upper)
+	func_root = func(root)
+	if abs(func_root) < eps:
+		if verbose: 
+			print("Root found at %.16f after %d iterations." % (root, iters))
+			return root, iters
+		return root
+#	else:
+	iters += 1
+	return secant(func, upper, root, eps, verbose, iters)
