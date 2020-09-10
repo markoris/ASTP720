0f419cf7f146ce70c86da9005f06c024a6ad8797
Marko Ristic <mr3364@rit.edu>
2020-09-09 22:46:34 -0400

    added LU decomposition, inverse, and situational determinant

--- HW2/mat.py
+++ HW2/mat.py
@@ -59,9 +59,90 @@ class Matrix(object):
 	def trace(self):
 		if self.n_rows != self.n_cols:
 			print('Matrix is not square and therefore the trace is undefined.')
-			return
+			pass
 		else:
 			trace = 0
 			for idx in range(self.n_rows):
 				trace += self.values[idx, idx]
 			return trace
+
+	def ludecomp(self):
+		L, U = Matrix(self.n_rows, self.n_cols), Matrix(self.n_rows, self.n_cols)
+
+		for row in range(self.n_rows): 
+			for col in range(row, self.n_cols):  
+
+				temp = 0
+				for idx in range(row): 
+				    temp += L.values[row, idx] * U.values[idx, col]
+				
+				U.values[row, col] = self.values[row, col] - temp # no need to divide by l_ii since diagonals of lower matrix are forced to be 1
+  
+			for col in range(row, self.n_cols): 
+
+				if row == col: 
+					L.values[row, col] = 1 # force that lower diagonal entries are 1
+
+				else: 
+					temp = 0
+					for idx in range(row): 
+					    temp += L.values[col, idx] * U.values[idx, row]
+		
+					L.values[col, row] = int((self.values[col, row] - temp) / U.values[row, row])
+
+		return L, U
+
+	def invert(self):
+		'''
+		
+		'''
+		if self.n_rows != self.n_cols:
+			print('Matrix is not square and therefore inversion has been canceled.')
+			pass
+		identity = np.identity(self.n_rows)
+
+		for row in range(self.n_rows):
+			for col in range(row+1):
+				if row != 0:
+					identity[row:, col] -= self.values[row, col]*identity[row-1, col]
+					self.values[row:, col] -= self.values[row, col]*self.values[row-1, col]
+				if row != col: 
+					continue
+				identity[row, :] /= self.values[row, col]
+				self.values[row, :] /= self.values[row, col]
+
+		for row in reversed(range(self.n_rows)):
+			for col in range(self.n_rows):
+				if row >= col: continue
+				if self.values[row, col] != 0:
+					identity[row, :] -= identity[col, :]*self.values[row, col]
+					self.values[row, :] -= self.values[col, :]*self.values[row, col]
+
+		self.values = identity
+		pass
+
+
+	def determinant(self):
+		'''
+		only works in some simple cases
+		'''
+		if self.n_rows != self.n_cols:
+			print('Matrix is not square and therefore inversion has been canceled.')
+			pass
+		identity = np.identity(self.n_rows)
+
+		product = 1
+
+		for row in range(self.n_rows):
+			for col in range(row+1):
+				if row != 0:
+					identity[row, col] -= self.values[row, col]*identity[row-1, col]
+					self.values[row, col] -= self.values[row, col]*self.values[row-1, col]
+				if row != col: 
+					continue
+				identity[row, :] /= self.values[row, col]
+				product /= self.values[row, col]
+				self.values[row, :] /= self.values[row, col]
+
+		print(1./product)
+		pass
