5e05d6264317f713dc84387346840c56a83cc6cb
Marko <mr3364@rit.edu>
2020-09-23 21:06:00 -0400

    cleaning up code, adding plot descriptors and comments

--- HW4/tasks.py
+++ HW4/tasks.py
@@ -4,33 +4,32 @@ import odes
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rc('font', size=26)
+plt.rc('lines', linewidth=3)
+
 # --- Start of Task 1 ---
 
 def hydro_eq_wd(r, variables):
 	G = 6.67e-8 # cgs units
 	P, M_enc = variables
-	if P < 0:return np.array([0]) # weird behavior due to large time steps around rho = 1, forcing P=0 stops nans
+	if P < 0:return np.array([0]) # weird behavior due to large time steps around rho = 1, force early return when P < 0
 	rho = 2.*(P/(1.e13))**(0.6) # white dwarf pressure-density relation
 	dvars_dt = np.array([-(G*M_enc*rho)/(r**2), 4.*np.pi*(r**2)*rho]) 
 	return dvars_dt
 
-#radii = np.linspace(1e-2, 1.9e9+1e-2, 10) # cm
-
-#rs, ys = odes.rk4(hydro_eq_wd, radii, np.array([1e13*((1e4)/2.)**(5./3), 1e-6]), dt=1e6)
-
+# looping over densities to create M-R diagram
 for rho in np.logspace(4, 6, 10):
 
 	radii = np.linspace(1e-2, 6e9+1e-2, 10) # cm
 
 	rs, ys = odes.rk4(hydro_eq_wd, radii, np.array([5.4e9*((rho))**(5./3), 1e-12]), dt=1e6)
-	#print(rs.shape)
 
-	plt.scatter(rs[-1]/1e5, ys[-1]/2e33)
+	plt.scatter(rs[-1]/1.e5, ys[-1]/2e33, s=500, label='rho = %1.2e' % rho)
 
-	#plt.plot(rs, ys[np.linspace(0, 2598, 1300).astype(int)])
-	#plt.plot(rs, ys[np.linspace(1, 2599, 1300).astype(int)])
-	#plt.show()
-
+plt.title('White dwarf mass-radius curve')
+plt.xlabel('Radius (km)')
+plt.ylabel('Mass (M_sun)')
+plt.legend(fontsize='small')
 plt.show()
 
 #plt.plot(rs, ys[np.linspace(0, 19998, 10000).astype(int)])
@@ -45,8 +44,8 @@ def hydro_eq_ns(r, variables):
 	G = 6.67e-8 # cgs units
 	c = 2.99e10 # cm/s
 	P, M_enc = variables
-	if P < 0: return np.array([0]) # weird behavior due to large time steps around rho = 1, forcing P=0 stop nans
-	rho = (P/(5.4e9))**(0.6) # white dwarf pressure-density relation
+	if P < 0: return np.array([0]) # weird behavior due to large time steps around rho = 1, force early return when P < 0
+	rho = (P/(5.4e9))**(0.6) # neutron star pressure-density relation
 	dvars_dt = np.array([-(G*M_enc*rho)/(r**2)*(1+P/(rho*c**2))*(1+4.*np.pi*r**3*P/(M_enc*c**2))*(1-2*G*M_enc/(r*c**2))**(-1), 4.*np.pi*(r**2)*rho]) 
 	return dvars_dt
 
@@ -55,14 +54,13 @@ for rho in np.logspace(14, 16, 10):
 	radii = np.linspace(1e-2, 1e9+1e-2, 10) # cm
 
 	rs, ys = odes.rk4(hydro_eq_ns, radii, np.array([5.4e9*((rho))**(5./3), 1e-12]), dt=1e3)
-	#print(rs.shape)
 
-	plt.scatter(rs[-1]/1e5, ys[-1]/2e33, label='%1.2e' % rho)
+	plt.scatter(rs[-1]/1.e5, ys[-1]/2.e33, s=500, label='rho = %1.2e' % rho)
 
-	#plt.plot(rs, ys[np.linspace(0, 2598, 1300).astype(int)])
-	#plt.plot(rs, ys[np.linspace(1, 2599, 1300).astype(int)])
-	#plt.show()
-plt.legend()
+plt.title('Neutron star mass-radius curve')
+plt.xlabel('Radius (km)')
+plt.ylabel('Mass (M_sun)')
+plt.legend(fontsize='small')
 plt.show()
 
 #radii = np.linspace(1e-2, 2e6+1e-2, 10) # cm
@@ -83,32 +81,16 @@ def hydro_eq_tov(r, variables):
 	G = 6.67e-8 # cgs units
 	c = 2.99e10 # cm/s
 	P, M_enc = variables
-	if P < 0: return np.array([0])
-#		P = 1e-10 # weird behavior due to large time steps around rho = 1, forcing P=0 stop nans
-	rho = (P/(5.4e9))**(0.6) # white dwarf pressure-density relation
+	if P < 0: return np.array([0]) # weird behavior due to large time steps around rho = 1, force early return when P < 0
+	rho = (P/(5.4e9))**(0.6) # neutron star pressure-density relation
 	dvars_dt = np.array([-(G*M_enc*rho)/(r**2)*(1+P/(rho*c**2))*(1+4.*np.pi*r**3*P/(M_enc*c**2))*(1-2*G*M_enc/(r*c**2))**(-1), 4.*np.pi*(r**2)*rho]) 
-# bottom pressure derivative is when you include terms of O(c^2) which actually recovers a mass close to what was found 
-#	dvars_dt = np.array([-(G*M_enc*rho)/(r**2)*(1 + P/(rho*c**2) + 4.*np.pi*r**3*P/(M_enc*c**2) + 2*G*M_enc/(r*c**2)), 4.*np.pi*(r**2)*rho]) 
 	return dvars_dt
 
-for rho in np.logspace(14, 16, 10):
+radii = np.linspace(1e-2, 1.302e6+1e-2, 10) # cm, going out to 13.02 km to get the mass of the neutron star
 
-	radii = np.linspace(1e-2, 1e9+1e-2, 10) # cm
+rs, ys = odes.rk4(hydro_eq_tov, radii, np.array([5.4e9*((1.03e15))**(5./3), 1e-12]), dt=1e3) # using density inferred from task 2 M-R curve
 
-	rs, ys = odes.rk4(hydro_eq_tov, radii, np.array([5.4e9*((rho))**(5./3), 1e-12]), dt=1e3)
-	#print(rs.shape)
-
-	plt.scatter(rs[-1]/1e5, ys[-1]/2e33)
-
-	#plt.plot(rs, ys[np.linspace(0, 2598, 1300).astype(int)])
-	#plt.plot(rs, ys[np.linspace(1, 2599, 1300).astype(int)])
-	#plt.show()
-
-plt.show()
-
-#print(ys.shape)
-
-#print(ys[-1]/2e33)
+print(rs.shape[0], rs[-1]/1e5, ys[-1]/1.989e33)
 
 
 # --- End of Task 3 ---
