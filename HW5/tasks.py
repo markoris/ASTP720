7a2623e031b0e0b0f022d303227697550b99c013
Marko Ristic <mr3364@rit.edu>
2020-10-11 11:19:24 -0400

    added awful attempt at rough potential calculation

--- HW5/tasks.py
+++ HW5/tasks.py
@@ -1,6 +1,9 @@
+# --- Start of Task 1 ---
+
 import barnes_hut as bh
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def verlet(x0, x1, y0, y1, t, nodes):
 	'''
@@ -38,7 +41,7 @@ for _ in range(10):
 
 	leaves = box.accel_calc()
 
-	x_next, y_next = verlet(xs[-2, :], xs[-1, :], ys[-2, :], ys[-1, :], 1000, leaves)
+	x_next, y_next = verlet(xs[-2, :], xs[-1, :], ys[-2, :], ys[-1, :], 10000, leaves)
 
 	idxs = np.where((x_next < 0) | (x_next > 10) | (y_next < 0) | (y_next > 10))
 
@@ -55,3 +58,33 @@ for _ in range(10):
 	plt.scatter(xs[-1, :], ys[-1, :])
 	plt.savefig('figures/step%d.png' % xs.shape[0])
 	plt.close()
+
+# --- End of Task 1 ---
+
+# --- Start of Task 2 ---
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xs, ys = np.linspace(0, 10, 10), np.linspace(0, 10, 10) # using REALLY rough resolution because higher resolution (only 10 pts) starts to show peaks at 7, 2 and 8, 7 which are artifacts of how I'm calculating the potential (which is clearly incorrect)
+
+X, Y = np.meshgrid(xs, ys)
+
+phi = np.zeros_like(X)
+
+G = 4.301e-3
+
+for x in range(X.shape[0]):
+	for y in range(Y.shape[0]):
+		phi1x = G * 400*1e12 * (-1/np.sqrt((X[x, y]-7)**2 + 1e-3**2)) # group of 400 galaxies at 7, 7
+		phi1y = G * 400*1e12 * (-1/np.sqrt((Y[x, y]-7)**2 + 1e-3**2))
+		phi1 = np.sqrt(phi1x**2 + phi1y**2)
+		phi2x = G * 255*1e12 * (-1/np.sqrt((X[x, y]-8)**2 + 1e-3**2)) # group of 255 galaxies at 8, 2
+		phi2y = G * 255*1e12 * (-1/np.sqrt((Y[x, y]-2)**2 + 1e-3**2))
+		phi2 = np.sqrt(phi2x**2 + phi2y**2)
+		phi[x, y] = phi1 + phi2 # adding potentials together
+
+plt.contour(X, Y, np.log10(phi))
+plt.show()
+
+# --- End of Task 2
