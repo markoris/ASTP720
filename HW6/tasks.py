34a40b302cbc28bfb2130921bed41c1032d73c9d
Marko Ristic <mr3364@rit.edu>
2020-10-15 18:40:40 -0400

    adjusting distance from kpc to pc

--- HW6/tasks.py
+++ HW6/tasks.py
@@ -8,8 +8,9 @@ from scipy.stats import f
 p, d, v, j, h, k, e, z = np.loadtxt('cepheid_data.txt', usecols=np.arange(1, 9, 1), delimiter=',', unpack=True)
 
 p = np.log10(p) # take log of period
+d *= 1e3 # changing distance from kpc to pc
 a_j = 3.1*(0.271)*e # calculate extinction coefficient
-m_j = j + 5 - 5*np.log10(d) - a_j # obtain absolute magnitude 
+m_j = j + 5 - 5*np.log10(d) - a_j # obtain absolute magnitude
 
 #plt.scatter(p, m_j) # verifying that trend resembles that in Fig 2 on hw
 #plt.gca().invert_yaxis()
