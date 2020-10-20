3e181e3beef3360fb5c9ff1a7259183776acc9b1
Marko Ristic <mr3364@rit.edu>
2020-10-20 12:03:02 -0400

    using likelihood to calculate metropolis ratio, not working well

--- HW7/tasks.py
+++ HW7/tasks.py
@@ -1,25 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#def likelihood(times, fluxes):
-#	return 1./np.sqrt(2*np.pi)*np.exp(-0.5*(fluxes-model(times))**2)
+def likelihood(times, fluxes, params):
+	sigma = 0.1
+	residual = fluxes - model(times, params)
+	residual /= sigma
+	residual = -0.5*np.sum(residual**2)
+	residual = np.exp(residual)
+	residual *= 1/np.sqrt(2*np.pi*sigma**2)
+	return residual
+#	return 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-0.5*np.sum(((fluxes - model(times, params))/sigma)**2))
+#	return 1/np.sqrt(2*np.pi)*np.exp(-0.5*np.sum((fluxes-model(times, params))**2/fluxes))
 
-def gaussnorm(x):
-	'''
-	standard normal distribution
-	'''
-	return 1/np.sqrt(2*np.pi)*np.exp(-(x)**2/(2))
-
-def model(times, dip, tmin, tmax):
+def model(times, params):
 	'''
 	transit model using simple inverted boxcar
 	'''
 	out = np.zeros_like(times)
-	idxs_flat = np.where((times < tmin) | (times > tmax))
-	idxs_dip = np.where((times > tmin) & (times < tmax))
+	idxs_flat = np.where((times < params[1]) | (times > params[2]))
+	idxs_dip = np.where((times > params[1]) & (times < params[2]))
 	out[idxs_flat] = 1
-	out[idxs_dip] = 1 - dip
+	out[idxs_dip] = 1 - params[0]
 	return out
+
+# --- Creating folded light curve --- 
 
 p = 3.5485 # days
 
@@ -47,36 +51,38 @@ lcbins[idxs] /= counter[idxs]
 
 foldtime = time[:nbins]
 foldtime, lcbins = foldtime[idxs], lcbins[idxs]
-#foldtime -= time[np.argmin(lcbins)]
+
+# --- Using Metropolis-Hastings to find optimal parameters
 
 dip = 0.007
 tmin = 2
 tmax = 2.3
 
-sim = model(foldtime, dip, tmin, tmax)
+params = np.array([dip, tmin, tmax])
+
+sim = model(foldtime, params)
 
 plt.plot(foldtime, lcbins)
 plt.plot(foldtime, sim)
 plt.show()
 
-params = np.array([dip, tmin, tmax])
+for run in range(10):
+	proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector
 
-for run in range(1000):
-	proposal = np.random.multivariate_normal(params, np.identity(3)*1e-6, 1).flatten() # this is the Y vector
+	while (np.any(proposal < 0)) | (proposal[2] < proposal[1]) | (proposal[2] > foldtime[-1]):
+		proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector
 
-	r = gaussnorm(proposal)/gaussnorm(params)
+	r = likelihood(foldtime, lcbins, proposal)/likelihood(foldtime, lcbins, params)
 
-	for value in range(len(r)):
-		if r[value] >= 1:
-			params[value] = proposal[value]
-		if r[value] < 1:
-			threshold = np.minimum(1, r[value])
-			check = np.random.uniform(0, 1, 1)
-			if check <= threshold:
-				params[value] = proposal[value]
+	if r >= 1: params = proposal
+	if r < 1:
+		threshold = np.minimum(1, r)
+		check = np.random.uniform(0, 1, 1)
+		if check <= threshold:
+			params = proposal
 
 print(params)
 
 plt.plot(foldtime, lcbins)
-plt.plot(foldtime, model(foldtime, params[0], params[1], params[2]))
+plt.plot(foldtime, model(foldtime, params))
 plt.show()
