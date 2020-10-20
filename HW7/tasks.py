9feb5fd3f7c10f873c21791f7b8eecde5c8ce821
Marko Ristic <mr3364@rit.edu>
2020-10-20 12:15:03 -0400

    comments for clarity

--- HW7/tasks.py
+++ HW7/tasks.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def likelihood(times, fluxes, params):
+	'''
+	gaussian likelihood evaluating how well model represents data
+	'''
 	sigma = 0.1
 	residual = fluxes - model(times, params)
 	residual /= sigma
@@ -9,8 +12,6 @@ def likelihood(times, fluxes, params):
 	residual = np.exp(residual)
 	residual *= 1/np.sqrt(2*np.pi*sigma**2)
 	return residual
-#	return 1/np.sqrt(2*np.pi*sigma**2)*np.exp(-0.5*np.sum(((fluxes - model(times, params))/sigma)**2))
-#	return 1/np.sqrt(2*np.pi)*np.exp(-0.5*np.sum((fluxes-model(times, params))**2/fluxes))
 
 def model(times, params):
 	'''
@@ -54,31 +55,31 @@ foldtime, lcbins = foldtime[idxs], lcbins[idxs]
 
 # --- Using Metropolis-Hastings to find optimal parameters
 
-dip = 0.007
-tmin = 2
-tmax = 2.3
+dip = 0.007 # initial guess at dip of transit
+tmin = 2    # initial guess at start of transit
+tmax = 2.3  # initial guess at end of transit
 
 params = np.array([dip, tmin, tmax])
 
-sim = model(foldtime, params)
+sim = model(foldtime, params) # boxcar representation of model given initial guess of parameters
 
 plt.plot(foldtime, lcbins)
 plt.plot(foldtime, sim)
-plt.show()
+plt.show() # pretty good! could be better...
 
 for run in range(10):
-	proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector
+	proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector, drawing from 3 different distributions, each with a mean at the current guess, with covariances = 1 on the diagonal
 
-	while (np.any(proposal < 0)) | (proposal[2] < proposal[1]) | (proposal[2] > foldtime[-1]):
-		proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector
+	while (np.any(proposal < 0)) | (proposal[2] < proposal[1]) | (proposal[2] > foldtime[-1]): # ensure that all values must be positive, tmax must be > tmin, and tmax should not be greater than the length of the relevant portion of the folded light curve
+		proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten()          # if any of above conditions are not met, re-draw samples until they are
 
-	r = likelihood(foldtime, lcbins, proposal)/likelihood(foldtime, lcbins, params)
+	r = likelihood(foldtime, lcbins, proposal)/likelihood(foldtime, lcbins, params) # metropolis ratio, ratio of likelihood given new proposed parameters to likelihood of current proposed parameters
 
-	if r >= 1: params = proposal
-	if r < 1:
-		threshold = np.minimum(1, r)
-		check = np.random.uniform(0, 1, 1)
-		if check <= threshold:
+	if r >= 1: params = proposal # if ratio > 1, proposed parameters are more likely to fit the data than current parameters, set current parameters to be the proposed ones
+	if r < 1: 
+		threshold = np.minimum(1, r) # otherwise, use the ratio as the threshold for acceptance
+		check = np.random.uniform(0, 1, 1) # draw from uniform sample
+		if check <= threshold: # if random draw is less than the threshold, accept the parameters, otherwise keep parameters same and draw another proposed set of parameters on the next iteration
 			params = proposal
 
 print(params)
