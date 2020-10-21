542ecbe2590cbafd47c8771ce1d86f45e33d741c
Marko Ristic <mr3364@rit.edu>
2020-10-21 15:09:09 -0400

    fixing likelihood and diagnostic plots

--- HW7/tasks.py
+++ HW7/tasks.py
@@ -5,13 +5,15 @@ def likelihood(times, fluxes, params):
 	'''
 	gaussian likelihood evaluating how well model represents data
 	'''
-	sigma = 0.1
+	sigma = 1
 	residual = fluxes - model(times, params)
 	residual /= sigma
+	residual = residual**2
 	residual *= -0.5
 	residual = np.exp(residual)
-	residual *= 1/np.sqrt(2*np.pi*sigma) # Gaussian for array of times and fluxes
+	residual *= 1/np.sqrt(2*np.pi*sigma**2) # Gaussian for array of times and fluxes
 	residual = np.sum(np.log(residual))  # log of Gaussian allows for summing over likelihood
+	residual = np.exp(residual)
 	return residual
 
 def model(times, params):
@@ -62,28 +64,55 @@ tmax = 2.3  # initial guess at end of transit
 
 params = np.array([dip, tmin, tmax])
 
+print(likelihood(foldtime, lcbins, params), likelihood(foldtime, lcbins, np.array([0.5, 2, 2.3])))
+
 sim = model(foldtime, params) # boxcar representation of model given initial guess of parameters
 
 plt.plot(foldtime, lcbins)
 plt.plot(foldtime, sim)
-plt.show() # pretty good! could be better...
+#plt.show() # pretty good! could be better...
+plt.close()
+
+cov_mat = np.identity(3)
+cov_mat[0, 0] = dip/10.
+cov_mat[1, 1] = tmin/10.
+cov_mat[2, 2] = tmax/10.
+
+dips = np.array([dip])
+tmins = np.array([tmin])
+tmaxs = np.array([tmax])
 
 for run in range(1000):
-	proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten() # this is the Y vector, drawing from 3 different distributions, each with a mean at the current guess, with covariances = 1 on the diagonal
+
+	proposal = np.random.multivariate_normal(params, cov_mat, 1).flatten() # this is the Y vector, drawing from 3 different distributions, each with a mean at the current guess, with covariances = 1 on the diagonal
 
 	while (np.any(proposal < 0)) | (proposal[2] < proposal[1]) | (proposal[2] > foldtime[-1]): # ensure that all values must be positive, tmax must be > tmin, and tmax should not be greater than the length of the relevant portion of the folded light curve
-		proposal = np.random.multivariate_normal(params, np.identity(3), 1).flatten()          # if any of above conditions are not met, re-draw samples until they are
+		proposal = np.random.multivariate_normal(params, cov_mat, 1).flatten()          # if any of above conditions are not met, re-draw samples until they are
 
 	r = likelihood(foldtime, lcbins, proposal)/likelihood(foldtime, lcbins, params) # metropolis ratio, ratio of likelihood given new proposed parameters to likelihood of current proposed parameters
 
-	print(r)
+#	print(params, likelihood(foldtime, lcbins, params))
+#	print(proposal, likelihood(foldtime, lcbins, proposal))
+#	print(r)
 
 	if r >= 1: params = proposal # if ratio > 1, proposed parameters are more likely to fit the data than current parameters, set current parameters to be the proposed ones
+	dips = np.append(dips, params[0])
+	tmins = np.append(tmins, params[1])
+	tmaxs = np.append(tmaxs, params[2])
 	if r < 1: 
 		threshold = np.minimum(1, r) # otherwise, use the ratio as the threshold for acceptance
 		check = np.random.uniform(0, 1, 1) # draw from uniform sample
 		if check <= threshold: # if random draw is less than the threshold, accept the parameters, otherwise keep parameters same and draw another proposed set of parameters on the next iteration
 			params = proposal
+			dips = np.append(dips, params[0])
+			tmins = np.append(tmins, params[1])
+			tmaxs = np.append(tmaxs, params[2])
+
+#plt.hist(dips, bins=50)
+#plt.show()
+
+#plt.plot(np.linspace(1, dips.shape[0], dips.shape[0]), dips)
+#plt.show()
 
 print(params)
 
