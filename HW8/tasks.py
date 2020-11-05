362fa93ec205818a8639123a36f4fd129136d2e0
Marko Ristic <mr3364@rit.edu>
2020-11-04 21:06:27 -0500

    added recursive element to FFT and achieved significant speed-up

--- HW8/tasks.py
+++ HW8/tasks.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 mpl.rcParams['agg.path.chunksize'] = 10000
 import matplotlib.pyplot as plt
 
+plt.rc('font', size=40)
+
 def time2freq(time):
 	samp_time = np.mean(np.diff(time))
 	samp_freq = 1/samp_time
@@ -18,16 +20,20 @@ def dft(inputs):
 
 def fft(inputs):
 	'''
-	optimized FFT using only 2 DFTs and a calculation of w
+	optimized FFT using recursive function calls and a pre-calculated w factor
 	'''
 	N = inputs.shape[0]
-	w = np.exp(-2j*np.pi/N)
-	idxs = np.arange(0, np.floor(N/2).astype(int)-1)
-	fft_even, fft_odd = np.zeros(idxs.shape[0]), np.zeros(idxs.shape[0])
-	for i in idxs:
-		fft_even[i] = np.sum([inputs[2*idx]*np.power(w, 2*i*idx) for idx in idxs])
-		fft_odd[i] = np.sum([inputs[2*idx+1]*np.power(w, 2*i*idx)*np.power(w, i) for idx in idxs])
-	return np.real(fft_even+fft_odd)
+	if N <= 1: return dft(inputs) # if array length below some threshold, just do a regular DFT. Using 1 since this matched the DFT best for the sine wave consistency check
+
+	idxs = np.arange(N//2) # linearly spaced N/2 index numbers for odd and even number identification
+
+	fft_even = fft(inputs[idxs*2]) # using even indices of the fft
+	fft_odd = fft(inputs[(idxs*2)+1]) # using odd indices of the fft
+
+	w = [np.exp(-2j*np.pi*k/N) for k in range(N)] # calculate w for each of the indices
+
+	return np.append(fft_even + w[:N//2]*fft_odd, fft_even + w[N//2:]*fft_odd) # appending second half to ensure that array size is maintained throughout recursion
+
 
 ## --- Start of Consistency Test --- #
 #
@@ -40,14 +46,13 @@ def fft(inputs):
 #
 #good_fft  = np.real(np.fft.fft(sine)[:freq.shape[0]])
 #
-#transform = fft(sine)
+#sine_copy = np.copy(sine)
 #
-#nested = nest_fft(sine)
+#transform = fft(sine_copy)
 #
 #plt.plot(freq, sine_fft, 'k')
 #plt.plot(freq, good_fft, 'r')
-#plt.plot(freq, transform, 'b')
-#plt.plot(freq, nested[:511], 'g')
+#plt.plot(freq, transform[:511], 'b')
 #plt.show() # all 3 methods should produce roughly the same output
 #
 ## --- End of Consistency Test --- #
@@ -57,31 +62,31 @@ def fft(inputs):
 strain = np.load('strain.npy') # strain measurements, one minute apart
 time = np.arange(strain.shape[0])*60 # seconds!
 
-freq = time2freq(time)
-#transform = fft(strain)
-transform = np.fft.fft(strain) # using numpy's fft because mine takes forever, not nested yet...
-transform = np.real(transform[:freq.shape[0]])
+freq = time2freq(time) # converting time to frequency domain
+transform = fft(strain) # calculating fft of strain data
+transform = np.real(transform[:freq.shape[0]])*2 # using only relevant part of FFT data and conserving variance by multiplying one-sided spectrum by 2
 
 plt.yscale('log')
 plt.xscale('log')
 plt.plot(freq, transform)
-#plt.show()
+plt.title('Cooley-Tukey FFT of strain data')
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Amplitude')
+plt.show()
 
-offset = np.where(freq > 0.001)[0][0]
+offset = np.where(freq > 0.001)[0][0] # index offset for finding the location of the peak in the entire array, not just the subset searched
 
 peak_freq = freq[np.argmax(transform[np.where((freq > 0.001) & (freq < 0.004))])+offset] # limit between these frequencies, otherwise it'll catch the larger amplitude stuff at higher frequencies
 peak = transform[np.argmax(transform[np.where((freq > 0.001) & (freq < 0.004))])+offset]
 peak /= strain.shape[0] # divide by N to normalize amplitude
-peak *= 2 # using one-sided spectrum, so multiply by 2 to conserve information
-
-print(peak)
 
 peak_freq /= 1e-4 # this value is equal to sqrt(M/M_sun) * (R/R_sun)^(-3/2) or M^1/2 = freq * R^3/2
 peak /= 2.6e-21 # this value is equal to (M/M_sun)^2 * (D/pc)^(-1) * (R/R_sun)^(-1)
 peak *= 12 # this  value is (M/M_sun)^2 (R/R_sun)^(-1), or alternatively, M^2 = peak * R
 peak = np.power(peak, 1./4) # now, M^1/2 = peak * R^1/4, therefore peak * R^1/4 = freq * R^3/2 -> peak * R^1/4 = freq * R^6/4 -> peak/freq = R^5/4, R = np.power(peak/freq, 4/5)
-R = np.power(peak/peak_freq, 4./5)
-M = np.sqrt(peak*R)
-print(M, R)
+R = np.power(peak/peak_freq, 4./5) # R = np.power(peak/freq, 4/5)
+M = np.sqrt(np.power(peak, 4)*R) # Since M^2 = peak * R, M = sqrt(peak*R), however this is the peak value before any modifications for finding separation R
+
+print("mass =", M, "M_sun, separation =", R, "R_sun.")
 
 # --- End of Task 1 --- #
